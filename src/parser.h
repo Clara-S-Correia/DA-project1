973fc547213a8d805b31d8958692b4bf2351c5b2
Esha-Naveed <up202400330@g.uporto.pt>
2026-03-29 18:31:27 +0100

    deleted the struct created in parser.h - just figured I am not using it. I made it to use it but not I am not so I deleted it

--- src/parser.h
+++ src/parser.h
@@ -3,13 +3,6 @@
 
 #include "dataStructures.h"//to use the sub and rev
 
-//data structure for reading data in general
-typedef struct {
-    int id;
-    int primary;
-    int secondary;
-} filedata_t;
-
 void parser(const std::string& filename, projectData& data);
 
 #endif //DA_PROJECT1_PARSER_H
